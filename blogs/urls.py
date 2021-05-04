f34b084fb3717c81a400addc4f2382446a3b1026
carinfinity <83638176+carinfinity@users.noreply.github.com>
2021-05-04 17:32:35 +0530

    Update urls.py

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     
     path('newcar/', views.newCar),
     path('upcomingcar/', views.upcomingCar)
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 ]
