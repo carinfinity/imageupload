6a6e39aac44d73096206f62507151ae7f7e7aee4
carinfinity <carinfinity.contact@gmail.com>
2021-05-04 17:20:48 +0530

    Add all my files

--- carinfinity/urls.py
+++ carinfinity/urls.py
@@ -23,4 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('blogs.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
